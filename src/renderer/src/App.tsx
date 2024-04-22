d6ca32fe84aafa1f7a6ac017b1f2856d328baaec
杜聪聪 <ducongcong@elead.com>
2024-04-22 19:27:44 +0800

    feat: Add an editor component

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,6 +1,13 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 
 const App = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+      navigate('/project')
+  }, [])
+
   return (
     <>
       <div>
---
+++ src/renderer/src/components/editor/index.tsx
@@ -0,0 +1,7 @@
+const Editor = () => {
+    return (
+        <div>这是编辑器</div>
+    );
+};
+
+export default Editor;
